ead0f97c744bb9c5bd6b05c5a195ff6da5c5778c
sfood-jjh <jh.jeong@s-food.com>
2025-11-17 13:54:31 +0900

    feat: add ESLint and Prettier configuration files

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,9 +1,9 @@
 import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import turboPlugin from 'eslint-plugin-turbo';
 import tseslint from 'typescript-eslint';
 import onlyWarn from 'eslint-plugin-only-warn';
 import eslintPluginPreferArrowFunctions from 'eslint-plugin-prefer-arrow-functions';
+import prettierConfig from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 /**
  * A shared ESLint configuration for the repository.
@@ -12,16 +12,29 @@ import eslintPluginPreferArrowFunctions from 'eslint-plugin-prefer-arrow-functio
  * */
 export const config = [
   js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
+  prettierConfig,
   {
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
     plugins: {
-      turbo: turboPlugin,
+      prettier: eslintPluginPrettier,
     },
     rules: {
-      'turbo/no-undeclared-env-vars': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+          bracketSpacing: true,
+          arrowParens: 'always',
+          printWidth: 100,
+          tabWidth: 2,
+          semi: true,
+          endOfLine: 'lf',
+        },
+      ],
     },
   },
+  ...tseslint.configs.recommended,
   {
     plugins: {
       onlyWarn,
